844d719e16578857ebe8fb47413f21ea78c7b103
mgasoftware <macir.gassama12@gmail.com>
2022-07-20 11:13:43 +0200

    Add step 8 begin

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -7,13 +7,28 @@ function mediaFactory(data) {
     function getUserMediaPicture() {
         const article = document.createElement('article');
         const div = document.createElement('div');
+        const divMediaInfos = document.createElement('div');
+        const divLikeHeart = document.createElement('div');
+        const pTitle = document.createElement('p');
+        const pLikes = document.createElement('p');
+        const i = document.createElement('i');
+
         div.className = "media";
+        divMediaInfos.className = "media-infos";
+        divLikeHeart.className = "media-likes";
+        pTitle.textContent = title;
+        pLikes.textContent = likes;
+        i.classList.add('fa-solid'); 
+        i.classList.add('fa-heart');
+
         if (image !== undefined) {
             const img = document.createElement('img');
+
             img.setAttribute("src", picture);
             img.setAttribute("alt", title);
             img.setAttribute('id', id);
             img.setAttribute('onclick', 'Lightbox.init()');
+
             div.appendChild(img);
         }
         else {
@@ -21,9 +36,15 @@ function mediaFactory(data) {
             const src = document.createElement('source');
             vid.setAttribute('src', videoMedia);
             vid.setAttribute('onclick', 'Lightbox.init()');
+            vid.setAttribute('alt', title);
             div.appendChild(vid);
         }
+        divMediaInfos.appendChild(pTitle);
+        divLikeHeart.appendChild(pLikes);
+        divLikeHeart.appendChild(i);
+        divMediaInfos.appendChild(divLikeHeart);
         article.appendChild(div);
+        article.appendChild(divMediaInfos);
         return (article);
     }
 
--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -2,18 +2,19 @@ class Lightbox {
     static init() {
         const links = Array.from(document.querySelectorAll('.media img, .media video'));
         const listMedias = links.map(link => link.getAttribute('src'));
+        const listTitles = links.map(link => link.getAttribute('alt'));
         links.forEach(link => link.addEventListener('click', e => {
             e.preventDefault();
-            new Lightbox(e.currentTarget.getAttribute('src'), listMedias, e.currentTarget.getAttribute('alt'));
+            new Lightbox(e.currentTarget.getAttribute('src'), listMedias, e.currentTarget.getAttribute('alt'), listTitles);
         }))
     }
 
-    constructor(url, listMedias, title) {
+    constructor(url, listMedias, title, listTitles) {
         this.element = this.buildDOM(url);
         this.listMedias = listMedias;
-        this.title = this.buildDOM(title);
+        this.listTitles = listTitles;
 
-        this.loadImage(url);
+        this.loadImage(url, title);
         this.onKeyUp = this.onKeyUp.bind(this);
 
         document.body.appendChild(this.element);
@@ -23,38 +24,53 @@ class Lightbox {
     loadImage(url, title) {
         let checkIMG = url.substr(-1);
         this.url = null;
+        this.title = null;
+
+        const lightbox = document.querySelector('.lightbox');
         const container = this.element.querySelector('.container-slides');
         const loader = document.createElement('div');
+
         loader.classList.add('lightbox_loader');
-        const titleMedia = document.createElement('h2');
-        titleMedia.classList.add('titre-media-lightbox');
+        const textMedia = document.createElement('h2');
+        textMedia.classList.add('titre-media-lightbox');
         container.innerHTML = '';
-        titleMedia.textContent = title;
-        container.appendChild(titleMedia);
+        main.setAttribute('aria-hidden', 'true');
+        lightbox.setAttribute('aria-hidden', 'false');
+
         container.appendChild(loader);
-        console.log(title);
+
         if (checkIMG !== '4') {
             const image = new Image();
             image.onload = () => {
                 container.removeChild(loader);
                 container.appendChild(image);
+                container.appendChild(textMedia);
                 this.url = url;
+                this.title = title;
             }
             image.src = url;
+            image.alt = title;
+            textMedia.textContent = title;
         }
         else {
             const vid = document.createElement('video');
             vid.setAttribute('autoplay', '');
             container.removeChild(loader);
             container.appendChild(vid);
+            container.appendChild(textMedia);
             this.url = url;
+            this.title = title;
             vid.src = url;
+            vid.alt = title;
+            textMedia.textContent = title;
         }
     }
 
     close(e) {
         e.preventDefault();
         const dom = document.getElementById('lightbox');
+        main.setAttribute('aria-hidden', 'false');
+        dom.setAttribute('aria-hidden', 'true');
         dom.style.display = 'none';
         document.removeEventListener('keyup', this.onKeyUp);
     }
@@ -62,19 +78,23 @@ class Lightbox {
     next(e) {
         e.preventDefault();
         let i = this.listMedias.findIndex(listMedia => listMedia === this.url);
+        let y = this.listTitles.findIndex(listTitle => listTitle === this.title);
         if (i === this.listMedias.length - 1) {
             i = -1;
+            y = -1;
         }
-        this.loadImage(this.listMedias[i + 1]);
+        this.loadImage(this.listMedias[i + 1], this.listTitles[y + 1]);
     }
 
     prev(e) {
         e.preventDefault();
         let i = this.listMedias.findIndex(listMedia => listMedia === this.url);
+        let y = this.listTitles.findIndex(listTitle => listTitle === this.title);
         if (i === 0) {
             i = this.listMedias.length;
+            y = this.listTitles.length;
         }
-        this.loadImage(this.listMedias[i - 1]);
+        this.loadImage(this.listMedias[i - 1], this.listTitles[y - 1]);
     }
 
     onKeyUp(e) {
@@ -94,10 +114,10 @@ class Lightbox {
                             <ul class="container-slides">
                             </ul>
                             <div class="commandes">
-                                <button class="left" aria-label="Previous media">
+                                <button class="left" aria-label="Previous image">
                                 <em class="fas fa-chevron-left"></em>
                             </button>
-                            <button class="right" aria-label="Next media">
+                            <button class="right" aria-label="Next image">
                                 <em class="fas fa-chevron-right"></em>
                             </button>
                             <button class="close" aria-label="Close dialog">
@@ -111,4 +131,3 @@ class Lightbox {
         return dom
     }
 }
-Lightbox.init();
